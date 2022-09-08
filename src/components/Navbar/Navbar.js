22f069107ea6344d7ae894f45954b269b16217de
Karandeep Singh <karandeepsandhu@Karandeeps-MacBook-Air.local>
2022-09-08 12:37:39 +0530

    Navbar Added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,28 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div className='navbar'>Navbar</div>
+        <div className='navbar w-100'>
+            <ul className='d-flex align-items-center p-3'>
+                <a href="#">
+                    <li className='mx-4'>Home</li>
+                </a>
+                <a href="#">
+                    <li className='mx-4'>Event-timeline</li>
+                </a>
+                <a href="#">
+                    <li className='mx-4'>Event cards</li>
+                </a>
+                <a href="#">
+                    <li className='mx-4'>Leads</li>
+                </a>
+                <a href="#">
+                    <li className='mx-4'>Sponsors</li>
+                </a>
+                <a href="#">
+                    <li className='mx-4'>Contacts</li>
+                </a>
+            </ul>
+        </div>
     )
 }
 
