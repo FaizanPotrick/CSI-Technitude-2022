210215441d7c731d44bd146110bcd04501b3812d
Karandeep Singh <karandeepsandhu@Karandeeps-MacBook-Air.local>
2022-09-08 14:12:42 +0530

    Navbar font updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <div className="hero"></div>
+      <div className="hero d-flex align-items-center justify-content-center">
+        <div className="hero-text text-center">
+          <h1>TECHNITUDE</h1>
+          <h1>2022</h1>
+        </div>
+      </div>
     </div >
   );
 }
