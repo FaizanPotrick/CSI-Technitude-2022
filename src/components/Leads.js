82a56e2d4d32817d92a766028632dbfc3a403e35
FaizanPotrick <faizan.potrick02@gmail.com>
2022-09-10 22:44:44 +0530

    Added Leads

--- src/components/Leads.js
+++ src/components/Leads.js
@@ -3,25 +3,42 @@ import leads from "../json/Leads.json";
 
 const Leads = () => {
   return (
-    <div className="flex flex-col items-center justify-center my-4">
-      <h2 className="mb-3">CSI DMCE</h2>
-      <div className="my-3 max-w-7xl flex flex-wrap gap-4 justify-center items-center">
-        {leads.map((lead, index) => {
-          return (
-            <div
-              className="flex flex-col items-center justify-end p-2 h-full"
-              key={index}
-            >
-              <img
-                className="h-28 w-28 sm:h-48 sm:w-48 object-cover"
-                src={require(`../assets/Leads/${lead.image}.png`)}
-                alt="lead name"
-              />
-              <div className="text-xl font-semibold">{lead.name}</div>
-              <div>{lead.position}</div>
-            </div>
-          );
-        })}
+    <div
+      id="leads"
+      className="flex flex-col items-center justify-center mt-10 py-5"
+    >
+      <div className="title">CSI-DMCE LEADS</div>
+      <div
+        className="flex flex-col items-center justify-center w-full px-3"
+        style={{
+          background: `url(${require("../assets/bg.png")})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "none",
+        }}
+      >
+        <div className="my-3 max-w-7xl flex flex-wrap gap-4 justify-center items-center px-3">
+          {leads.map((lead, index) => {
+            return (
+              <div
+                className="flex flex-col items-center justify-end p-2 h-full"
+                key={index}
+              >
+                <img
+                  className={`h-32 ${
+                    index === 0 || index === 1 ? "w-full" : "w-32"
+                  } sm:h-48 sm:w-48 object-contain`}
+                  src={require(`../assets/leads/${lead.image}.png`)}
+                  alt="lead name"
+                />
+                <div className="text-lg sm:text-xl font-semibold">
+                  {lead.name}
+                </div>
+                <div className="text-sm sm:text-base">{lead.position}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
