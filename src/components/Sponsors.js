7cb9469dabb1436cda0faa8dfb307ea0a87e868a
FaizanPotrick <faizan.potrick02@gmail.com>
2022-09-10 22:44:37 +0530

    Added Sponsors

---
+++ src/components/Sponsors.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sponsors = () => {
+  return (
+    <div
+      id="sponsors"
+      className="relative flex flex-col items-center justify-center mt-20 py-5 px-3"
+    >
+      <h2 className="title">Sponsors</h2>
+    </div>
+  );
+};
+
+export default Sponsors;
