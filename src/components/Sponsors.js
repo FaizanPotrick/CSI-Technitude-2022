58a3d1c09db89d29adb1a4d99f3c75bc3533e418
FaizanPotrick <faizan.potrick02@gmail.com>
2022-09-13 21:36:46 +0530

    Update Sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,19 +1,25 @@
 import React from "react";
-import Spons1 from '../assets/Sponsors/Farmhouse logo normal size.png'
-import Spons2 from '../assets/Sponsors/IMG-20220913-WA0005.jpg'
-import Spons3 from '../assets/Sponsors/IMG-20220913-WA0007.jpg'
 
 const Sponsors = () => {
   return (
-    <div
-      id="sponsors"
-      className="relative flex flex-col items-center justify-center mt-20 py-5 px-3"
-    >
+    <div className="relative flex flex-col items-center justify-center mt-20 py-5 px-3">
       <h2 className="title">Sponsors</h2>
-      <div className="flex sponsor-images">
-        <img src={Spons2} alt="" />
-        <img src={Spons1} alt="" />
-        <img src={Spons3} alt="" />
+      <div className="flex justify-evenly items-center w-full flex-wrap gap-4">
+        <img
+          src={require("../assets/sponsors/IMG-20220913-WA0005.jpg")}
+          alt=""
+          className="h-16 sm:h-20 md:h-32 rounded-lg"
+        />
+        <img
+          src={require("../assets/sponsors/Farmhouse logo normal size.png")}
+          alt=""
+          className="h-24 sm:h-32 md:h-44"
+        />
+        <img
+          src={require("../assets/sponsors/IMG-20220913-WA0007.jpg")}
+          alt=""
+          className="h-16 sm:h-20 md:h-32 rounded-lg"
+        />
       </div>
     </div>
   );
